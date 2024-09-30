74a3a1ac68758d2238d758664eac01540c497cec
sahil-tgs <sahiltgs@gmail.com>
2024-09-30 16:40:00 +0530

    feat(client): import react app skeleton

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        cream: '#FFFDF6',
+      },
+    },
+  },
+  plugins: [],
+}
