36d97e42ec6b28f0d2fe060020f9248d28e2e04d
sahil-tgs <sahiltgs@gmail.com>
2024-11-01 11:05:00 +0530

    feat(config): scaffold spring cloud config server

---
+++ config-server/src/main/java/com/iiitn/resell/config/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.iiitn.resell.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
