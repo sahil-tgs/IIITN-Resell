296414507c6bb2c64e7c125fd1512c873aac14e1
sahil-tgs <sahiltgs@gmail.com>
2025-01-23 20:10:00 +0530

    feat(product): cloudinary config

---
+++ product-service/src/main/java/com/iiitn/resell/product/config/CloudinaryConfig.java
@@ -0,0 +1,20 @@
+package com.iiitn.resell.product.config;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CloudinaryConfig {
+
+    @Bean
+    public Cloudinary cloudinary(
+            @Value("${cloudinary.cloud-name}") String name,
+            @Value("${cloudinary.api-key}")    String key,
+            @Value("${cloudinary.api-secret}") String secret) {
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", name, "api_key", key, "api_secret", secret, "secure", true));
+    }
+}
